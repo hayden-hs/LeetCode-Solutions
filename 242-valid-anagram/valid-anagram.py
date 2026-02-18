091dabc1ab95bb9b9d444544b45d6cea25e0f408
hayden-hs <hr2712@nyu.edu>
2026-02-17 22:35:30 -0500

    Time: 7 ms (90.82%) | Memory: 19.5 MB (32.82%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,7 +1,7 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        Counter(s) == Counter(t)
-        
+        return Counter(s) == Counter(t)
+
         if len(s) != len(t):
             return False
         
